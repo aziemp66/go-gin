b60b57b519ba8f5395a126e3813368748c1beb9e
Azie Melza Pratama <aziemp66@gmail.com>
2022-11-24 14:18:09 +0700

    Path Variable and Query String

--- main.go
+++ main.go
@@ -13,6 +13,10 @@ func main() {
 
 	router.GET("/hello", helloHandler)
 
+	router.GET("/books/:id/:title", booksHandler)
+
+	router.GET("/books", queryHandler)
+
 	router.Run(":3000")
 }
 
@@ -28,4 +32,25 @@ func helloHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"content": "Hello World",
 	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+
+	c.JSON(http.StatusOK, gin.H{
+		"id":    id,
+		"title": title,
+	})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	author := c.Query("author")
+
+	c.JSON(http.StatusOK, gin.H{
+		"title":  title,
+		"author": author,
+	})
+
 }
